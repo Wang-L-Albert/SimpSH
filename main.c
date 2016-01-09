9c246142daee895f499707bd6b8c3d21470b7f9d
Albert Wang <wang.l.albert@gmail.com>
2016-01-09 13:34:04 -0800

    Update 1
    = -> ==

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <getopt.h>
  
-int optid = 1; //tracks which read in argument we'll read next
-char* optarg; //will store the arguments for the option as we move it in.
+//int optid = 1; //tracks which read in argument we'll read next
+//char* optarg; //will store the arguments for the option as we move it in.
 
 
 
@@ -18,13 +18,17 @@ int main(int argc, char* argv[]){
 	int option_ind = 0;
 	while (1){ //*********8check if this is the correct limit
 		optVal = getopt_long(argc, argv, "", optionlist, &option_ind);
-		if (optVal = -1){
+		if (optVal == -1){
+			printf("%s\n", ("break"));
 			break;
 		}
+
 		//use optVal for switch statements, option_ind for access
 		switch (optVal) {
 			case 'r':
 				printf("%s\n", ("Found a rdonly option."));
+			default:
+				printf("%s\n", "No match found.");
 		}
 	}
 }
