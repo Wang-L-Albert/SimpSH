ca0e7fef60868e07f532390cd406a6c47c0ac2c4
Lauren Y <lyeung.ca@gmail.com>
2016-01-12 23:38:17 -0800

    Redirection works

--- main.c
+++ main.c
@@ -7,9 +7,6 @@
 #include <fcntl.h>
 //int optid = 1; //tracks which read in argument we'll read next
 //char* optarg; //will store the arguments for the option as we move it in.
-
-
-
 int main(int argc, char* argv[]){
 	//set up dynamic arrays
 	//there will never be more than argc child processes, so this is more than enough.
@@ -32,7 +29,7 @@ int main(int argc, char* argv[]){
 	int option_ind = 0;
 	while (1){ 
 		optVal = getopt_long(argc, argv, "", optionlist, &option_ind); //option_ind is the option's index in argv[].
-		//global var optarg now points to option_ind+1, and optind to the index of the next index (first non-option 
+		//global var optarg now points to potion_ind+1, and optind to the index of the next index (first non-option 
 		//in argv[] if no more options.
 		if (optVal == -1){
 			printf("%s\n", ("break on -1"));
@@ -51,6 +48,7 @@ int main(int argc, char* argv[]){
 			case 'w'://wronly
 				fidList[numFid++] = open(optarg, O_WRONLY);
 				printf("%s\n",("Opened in write only."));
+				printf("optarg: %s\n", optarg);
 				printf("%d\n", (fidList[numFid-1]));
 				break;
 			case 'v'://verbose
@@ -67,37 +65,60 @@ int main(int argc, char* argv[]){
 					//at this point, optarg points to the next arg, optind next ind
 					//need a variable for end of our stream of arguments
 					int optEnd = 0;
-
 					//need to check if any more options or not.
 					int tempOptInd = optind;
 					int tempOpt_Ind = 0;
 					getopt_long(argc, argv, "", optionlist, &tempOpt_Ind);
-					if (optind < tempOptInd){//would only be true if no more options and it went back to first non-opt arg
+					if (optind == tempOptInd){//would only be true if no more options and it went back to first non-opt arg
 						optEnd = argc;
 					} else {
 						optEnd = tempOpt_Ind;
+						optind = tempOptInd;//undo the search, we only want to verify there was another one. 
 					}
-					printf("optEnd miuw:%d/n", optEnd);
-					printf("optind mraoww:%d/n", optind);
 					//now three times from option_ind+1 to get the I/O re-routes
-					int argParse = option_ind+1;
+					int argParse = optind-1;
+					printf("option_ind: %d\n", option_ind);
+					printf("OptEnd: %d\n", optEnd);
+					printf("Optind: %d\n", optind);
+					printf("argParse: %d\n", argParse);
+					// dup2(3,0);
+					// dup2(5,2);
+					// printf("NOOOOOOOOOOOOOOOO 1\n" );
+					// dup2(4,1);
+					
 					for (int i = 0; i < 3; i++){
-						int newIO = (int) (argv[argParse++] - '0');////////cast from pointer to int of diff size
-						dup2(newIO, i-1);
+						printf("INSDE THE FOR LOOP 				argParse: %d\n", argParse);
+						printf("INSDE THE FOR LOOP 				filedescrip: %s\n", argv[argParse]);
+						int newIO = strtol(argv[argParse++], NULL, 10);
+						printf("newIO: %d\n", newIO );
+						//int newIO = (int) (argv[argParse++] - '0');////////cast from pointer to int of diff size
+						printf("FUCKKKKKKKKKKKKKKKKKKKKK%d\n", fidList[2]);
+						printf("fidlist shiiit real newIO: %d\n", fidList[newIO] );
+						dup2(fidList[newIO], i);						
 					}
 					//now that IO is rewritten, get the string
 					//argParse should now be 4, the position of the char* for our command
 					char* newCmd = argv[argParse++];
-					//now store the arguments for CMD in a new array.
-					int argSize = optEnd-argParse;
+					printf("passed in command: %s\n", newCmd);
 
-					char** newArgs;
-					*newArgs = (char*) malloc((argSize+1)*sizeof(char *));/////may be used uninit in this func
+					//now store the arguments for CMD in a new array.
+					int argSize = optEnd-argParse+1;
+					//printf("FUCK 1\n" );
+					char** newArgs = NULL;
+					//printf("FUCK 2\n" );
+					newArgs = (char*) malloc((argSize+1)*sizeof(char *));/////may be used uninit in this func
+					//printf("FUCK 3\n" );
 					int newArgIndex;
-					for (newArgIndex = 0; newArgIndex < argSize; newArgIndex++){
+					//printf("FUCK 4\n" );
+					printf("argSize:     %d\n", argSize);
+					newArgs[0]=newCmd;
+					for (newArgIndex = 1; newArgIndex < argSize; newArgIndex++){
 						newArgs[newArgIndex] = argv[argParse++];
+						printf("newArgs[newArgIndex] :        %s\n", newArgs[newArgIndex]);
 					}
+					printf("newCmd:     %s\n", newCmd);
 					//place nullptr at the end of the array.
+					printf("newArgIndex: %d\n", newArgIndex);
 					newArgs[newArgIndex] = NULL;
 					//carry out new execution
 					execvp(newCmd, newArgs);
